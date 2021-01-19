69f54798d5f94076b646a1c31170c91347726fa8
GITHUB Nexend <GITHUB ddante@ukr.net>
2021-01-19 18:42:20 +0300

    task5

---
+++ lesson3-components/task5-profile/src/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import moment from 'moment';
+
+const formatDate = date => moment(date).format('DD MMM YY');
+
+const Profile = props => {
+  return (
+    <div className="greeting">
+      <div className="profile__name">
+        {`${props.userData.firstName} ${props.userData.lastName}`}
+      </div>
+      <div className="profile__birth">
+        {`Was born ${formatDate(props.userData.birthDate)} in ${
+          props.userData.birthPlace
+        }`}
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
