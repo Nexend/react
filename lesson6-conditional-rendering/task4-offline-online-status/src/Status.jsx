a1b3db18e1131ad6a8fcc4ec92f20a82c0ba08a1
GITHUB Nexend <GITHUB ddante@ukr.net>
2021-01-21 18:26:23 +0300

    task4

---
+++ lesson6-conditional-rendering/task4-offline-online-status/src/Status.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Online from './Online.jsx';
+import Offline from './Offline.jsx';
+
+class Status extends Component {
+  state = {
+    isOnline: true,
+  };
+
+  render() {
+    return <div className="status">{this.state.isOnline ? <Online /> : <Offline />}</div>;
+  }
+}
+
+export default Status;
