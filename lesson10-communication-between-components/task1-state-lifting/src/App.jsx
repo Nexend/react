eeb19ad7251b96d0bfc8fb7af56d475beaa79b45
GITHUB Nexend <GITHUB ddante@ukr.net>
2021-01-26 14:53:28 +0300

    task1

---
+++ lesson10-communication-between-components/task1-state-lifting/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import UserProfile from './UserProfile.jsx';
+import UserMenu from './UserMenu.jsx';
+
+class App extends Component {
+  state = {
+    userData: null,
+  };
+
+  componentDidMount() {
+    this.fetchUser(this.props.userId);
+  }
+
+  fetchUser = userId => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          userData: data,
+        });
+      });
+  };
+  render() {
+    return (
+      <div className="page">
+        <header className="header">
+          <UserMenu userData={this.state.userData} />
+        </header>
+        <UserProfile userData={this.state.userData} />
+      </div>
+    );
+  }
+}
+
+export default App;
