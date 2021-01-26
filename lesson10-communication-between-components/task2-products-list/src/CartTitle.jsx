fd6729138a421e01c0fddebb047eb6d6272bf426
GITHUB Nexend <GITHUB ddante@ukr.net>
2021-01-26 16:01:03 +0300

    task2

---
+++ lesson10-communication-between-components/task2-products-list/src/CartTitle.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const CartTitle = ({ userName, count }) => {
+  return <div className="cart-title">{`${userName}, you added ${count} items`}</div>;
+};
+
+export default CartTitle;
